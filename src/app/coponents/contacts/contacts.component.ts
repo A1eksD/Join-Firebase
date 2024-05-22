441cbfe26e2cdccf58ad109a2347ae11fd6c8c53
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-05-22 16:20:29 +0200

    contact - add earchbar

--- src/app/coponents/contacts/contacts.component.ts
+++ src/app/coponents/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { User } from '../../interface/user';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -15,6 +15,7 @@ import { UsersService } from '../../service/users.service';
 })
 export class ContactsComponent {
   allUsersArray: any[] = [];
+  SortedContacts: any[] = [];
   name:string = '';
   email:string = '';
   phoneNr:any = undefined;
@@ -23,13 +24,22 @@ export class ContactsComponent {
   editPhoneNr:any = undefined;
   firstName:string = '';
   lastName:string = '';
+  firstChar:string = '';
+  headerInputValue:string = '';
   closeUserWindow:boolean = false;
   closeUserEditWindow:boolean = false;
   userDetails: any = '';
   getUserToEdit: any = '';
+  searchBarUsersArray: any[] = [];
+  searchBarUsersArraySorted: any[] = [];
+  noUserFound: boolean = false;
 
 
   constructor(public toggleService: ToggleBooleansService, public userService: UsersService){}
+
+  ngOnInit(){
+    this.showUserContacts();    
+  }
 
   getUserFirstLetter(user: User){
     return user?.firstName?.charAt(0).toUpperCase() || '';
@@ -53,6 +63,7 @@ export class ContactsComponent {
         uid: (Math.random() * 341235).toString(),
       };
       this.userService.addNewContact([user]);
+      this.showUserContacts();
       this.returnBack();
     }
   }
@@ -175,6 +186,115 @@ export class ContactsComponent {
   showUserContacts(){
     const currentUser = localStorage.getItem('currentUser');
     const filterUser = this.userService.allUsers.filter(u => u.id === this.userService.getCleanID(currentUser!))
-    return filterUser[0].savedUsers;
+    this.SortedContacts = this.sortByFirstLetter(filterUser[0].savedUsers);
+    return this.SortedContacts;
   }
+
+  sortByFirstLetter(contacts: User[]): User[] {
+    return contacts.sort((a, b) => {
+        const nameA = a.firstName.toUpperCase();
+        const nameB = b.firstName.toUpperCase();
+        return nameA.localeCompare(nameB);
+    });
+  }
+
+  getFirstCharacter(user: User): string | null {
+    const firstLetter = user.firstName.charAt(0).toUpperCase();
+    if (this.firstChar.includes(firstLetter)) {
+      return '';
+    }
+    this.firstChar = firstLetter;
+    return this.firstChar;
+  }
+  
+  getHeaderInputValue() {
+    const currentUser = localStorage.getItem('currentUser');
+    // Filter current user's contacts
+    const filteredUser = this.userService.allUsers.filter((u) => u.id === this.userService.getCleanID(currentUser!));
+    // Filter common users based on search input
+    const filterUserExistingUserName = filteredUser[0].savedUsers.filter((user: any) => (user.firstName.includes(this.headerInputValue)));
+    const sortedFilterUserExistingUserName = this.sortFilterUserExistingUserName(filterUserExistingUserName);
+    //------------------- 
+    const filterUserToAddName = this.userService.commonUsers.filter((user: any) => (user.savedUsers[0].firstName.includes(this.headerInputValue)));
+    const sortedFilterUserToAddName = this.sortFilterUserToAddName(filterUserToAddName);
+
+    // if (!this.isEqual(sortedFilterUserExistingUserName, sortedFilterUserToAddName)) {
+      this.checkDifferentUser(sortedFilterUserExistingUserName, sortedFilterUserToAddName);
+    // }
+  }
+
+  sortFilterUserExistingUserName(contacts: User[]): User[]{
+    return contacts.sort((a, b) => {
+      const nameA = a.firstName.toUpperCase();
+      const nameB = b.firstName.toUpperCase();
+      return nameA.localeCompare(nameB);
+    });
+  }
+
+  sortFilterUserToAddName(contacts: User[]): User[]{
+    return contacts.sort((a, b) => {
+      const nameA = a.savedUsers![0].firstName.toUpperCase();
+      const nameB = b.savedUsers![0].firstName.toUpperCase();
+      const getName = nameA.localeCompare(nameB);
+      this.searchBarUsersArraySorted.push(getName);
+      return getName;
+    });
+  }
+
+  
+  isEqual(arr1: any[], arr2: any[]): boolean {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+      if (typeof arr1[i] !== typeof arr2[i].savedUsers[i]) return false;
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i].savedUsers[i])) {
+        if (!this.isEqual(arr1[i], arr2[i].savedUsers[i])) return false;
+      } else if (arr1[i] !== arr2[i].savedUsers[i]) return false;
+    }
+    return true;
+  }
+
+  checkDifferentUser(filterUserExistingUserName: User[], filterUserToAddName: User[]) {
+    this.searchBarUsersArray = [];
+    if (this.headerInputValue !== '') {
+      for (let i = 0; i < filterUserToAddName.length; i++) {
+        // if (!this.checkUserFirstName()) {
+        //   this.searchBarUsersArray = [];
+        // } else 
+        if(filterUserExistingUserName[i] !== filterUserToAddName[i].savedUsers![0]) {
+          this.searchBarUsersArray.push(filterUserToAddName[i].savedUsers![0]);
+          console.log(this.searchBarUsersArray);
+        }
+      } 
+    } else {
+      this.searchBarUsersArray = [];
+    }
+  }
+
+  checkUserFirstName() {
+    const userFirstNameExists = this.searchBarUsersArray.some(user =>
+      user.firstName.toLowerCase().includes(this.headerInputValue.toLowerCase())
+    );
+  
+    if (userFirstNameExists) {
+      this.noUserFound = true;
+      return true;
+    } else {
+      this.noUserFound = false;
+      return false;
+    }
+  }
+  
+  
+  getSplitArray(savedUsers: User[]){
+
+  }
+
+  checkInputInSidebar(){
+    if (this.headerInputValue) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
 }
